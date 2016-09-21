94a163f5561c14ff2a023ec4e594465b2b5f2d88
jfine24 <jfine24@gmail.com>
2016-09-21 13:57:31 -0400

    initial coding (skeleton)

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+var fs = require('fs');
+var mime = require('mime');
+
+app.use(express.static('html'));
+app.use(express.static('js'));
+app.use(express.static('css'));
+app.use(express.static('img'));
+app.use(express.static('build'));
+app.use(cookieParser());
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var multer  = require('multer');
+
+app.get('/', function (req, res) {
+    res.redirect('/index.html');
+
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
