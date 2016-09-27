2a7b67796cf92fd6f24425e078668f5c7909ac58
JMelka <JMelka@verizon.net>
2016-09-27 14:19:24 -0400

    newListing

--- public/newListing/newListing.module.js
+++ public/newListing/newListing.module.js
@@ -1,23 +1,4 @@
 angular.module('newListing', [
-    'ngRoute'
+    'ngRoute',
+    'ngCookies'
 ]);
-
-// newListing.directive('fileModel', ['$parse', function ($parse) {
-//     return {
-//         restrict: 'A',
-//         link: function (scope, element, attrs) {
-//             var model = $parse(attrs.fileModel);
-//             var modelSetter = model.assign;
-
-//             element.bind('change', function () {
-//                 scope.$apply(function () {
-//                     console.log('bind apply here')
-//                     modelSetter(scope, element[0].files[0]);
-//                 });
-//             });
-//         }
-//     };
-// }]);
-
-
-
